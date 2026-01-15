76ecec64d407980f2292824d13e405b5b5fa7a62
Jose C. Santos <zderimanz@hotmail.com>
2026-01-15 20:14:29 +0100

    db schema added

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -5,7 +5,7 @@ dotenv.config({ quiet: true });
 export const ENV = {
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT || 3000,
-  DB_URL: process.env.DB_URL,
+  DATABASE_URL: process.env.DATABASE_URL,
   FRONTEND_URL: process.env.FRONTEND_URL,
   CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
   CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY
