1e9fca02da4c212ac452ad3677a35886e0448216
Jose C. Santos <zderimanz@hotmail.com>
2026-01-17 20:10:31 +0100

    Update queries

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 import { ENV } from "./src/config/env";
 
+// Setear el schema y la url de la base de datos
 export default defineConfig({
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
